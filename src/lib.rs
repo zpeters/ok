36ba92a1f3cfd9ddd4b0e70fcc080644127c98bd
Zach Peters <zpeters@gmail.com>
2019-03-13 13:12:42 -0500

    added example docs and tests

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+//! TODO
+//! - hardcode repos
+//! - implement list
+//! - implement go
+//! - implement go *
+//! - rust doc - see foo examples
+//! - unit tests - https://doc.rust-lang.org/rust-by-example/testing/unit_testing.html
+//! - dynamic config for repos
+
+/// This is a summary of the mod
+///
+/// This is a little more detailed description
+pub mod test_mod {
+    //! this is inner doc for test_mod
+
+    /// This is some info about the function foo
+    ///
+    /// # Examples
+    /// ```
+    /// assert!(ok::test_mod::foo());
+    /// ```
+    pub fn foo() -> bool {
+        println!("Foobar");
+        true
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 extern crate clap;
 
-use clap::{App, SubCommand, AppSettings, Arg};
+use clap::{App, AppSettings, Arg, SubCommand};
 
-fn main() {
+pub fn main() {
+    let repos = ["~/Projects/", "~/"];
+    println!("My Repos: {:? }", repos);
+
     let matches = App::new("Ok")
         .version("0.0.1")
         .author("Zach Peters")
         .about("Ok git helper")
         .setting(AppSettings::ColoredHelp)
         .setting(AppSettings::ArgRequiredElseHelp)
-        .subcommand(SubCommand::with_name("list")
-                    .about("lists all repos with 'changes'"))
-        .subcommand(SubCommand::with_name("go")
-                    .about("commit and push all 'changed' repos")
-                    .arg(Arg::with_name("repo")
-                         .multiple(true)))
+        .subcommand(SubCommand::with_name("list").about("lists all repos with 'changes'"))
+        .subcommand(
+            SubCommand::with_name("go")
+                .about("commit and push all 'changed' repos")
+                .arg(Arg::with_name("repo").multiple(true)),
+        )
         .get_matches();
 
     if matches.is_present("list") {
