70051f2fee9ec334d1826a3d9fdda62669e523dd
Zach Peters <zpeters@gmail.com>
2019-03-14 21:51:27 -0500

    added some testing error code

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,14 @@
 //! - unit tests - https://doc.rust-lang.org/rust-by-example/testing/unit_testing.html
 //! - dynamic config for repos
 
+extern crate failure;
+
 /// This is a summary of the mod
 ///
 /// This is a little more detailed description
 pub mod test_mod {
     //! this is inner doc for test_mod
+    use failure::Error;
 
     /// This is some info about the function foo
     ///
@@ -22,5 +25,10 @@ pub mod test_mod {
     pub fn foo() -> bool {
         println!("Foobar");
         true
+    }
+
+    pub fn error_test() -> Result<(), Error> {
+        Err(failure::err_msg("an error"))
+        //Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate clap;
+extern crate ok;
 
 use clap::{App, AppSettings, Arg, SubCommand};
+use ok::test_mod::{error_test, foo};
 
 pub fn main() {
     let repos = ["~/Projects/", "~/"];
@@ -21,7 +23,13 @@ pub fn main() {
         .get_matches();
 
     if matches.is_present("list") {
-        println!("Called list")
+        println!("Called list");
+        foo();
+        let resp = error_test();
+        match resp {
+            Ok(r) => println!("Success: {:?}", r),
+            Err(e) => println!("Error: {}", e),
+        }
     }
 
     if let Some(go_matches) = matches.subcommand_matches("go") {
